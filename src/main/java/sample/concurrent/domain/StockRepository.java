2d4c218c7b9bee9a81d9ba5d2ec666965dbacdd5
kdmin0706 <gxxk76@gmail.com>
2024-08-06 10:49:23 +0900

    [feat]: pessimistic Lock 구현    - 데이터에 실제 `Lock`을 걸어서 사용    - 실제 트랜잭션에서는 Lock이 해제되기 전까지는 데이터를 가지고 갈 수 없다.    - JPA Repository에 `@Lock` 어노테이션을 사용한다.

--- src/main/java/sample/concurrent/domain/StockRepository.java
+++ src/main/java/sample/concurrent/domain/StockRepository.java
@@ -1,7 +1,13 @@
 package sample.concurrent.domain;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 public interface StockRepository extends JpaRepository<Stock, Long> {
 
+  @Lock(LockModeType.PESSIMISTIC_WRITE)
+  @Query(" select s from Stock s where s.id = :id ")
+  Stock findByIdWithPessimisticLock(Long id);
 }
