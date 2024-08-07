b1bc7bfd28abf47f85e3c82a89555ea4618227e1
kdmin0706 <gxxk76@gmail.com>
2024-08-07 11:25:43 +0900

    [test]: 테스트 코드 작성

---
+++ src/test/java/sample/concurrent/api/facade/RedissonLockStockFacadeTest.java
@@ -0,0 +1,56 @@
+package sample.concurrent.api.facade;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import sample.concurrent.domain.Stock;
+import sample.concurrent.domain.StockRepository;
+
+@SpringBootTest
+class RedissonLockStockFacadeTest {
+
+  @Autowired
+  private StockRepository stockRepository;
+
+  @Autowired
+  private RedissonLockStockFacade redissonLockStockFacade;
+
+  @BeforeEach
+  void setUp() {
+    stockRepository.save(new Stock(1L, 100L));
+  }
+
+  @AfterEach
+  void tearDown() {
+    stockRepository.deleteAllInBatch();
+  }
+
+  @Test
+  public void 동시에_100개의_요청() throws InterruptedException {
+    int threadCount = 100;  //100개의 요청을 보냄
+    ExecutorService executorService = Executors.newFixedThreadPool(32);
+    CountDownLatch latch = new CountDownLatch(threadCount);
+
+    for (int i = 0; i < threadCount; i++) {
+      executorService.submit(() -> {
+        try {
+          redissonLockStockFacade.decrease(1L, 1L);
+        } finally {
+          latch.countDown();
+        }
+      });
+    }
+
+    latch.await();
+
+    Stock stock = stockRepository.findById(1L).orElseThrow();
+    assertThat(stock.getQuantity()).isZero();
+  }
+}
